b03b0bffd4b1697202754b9fe05651776ab923d3
Shiv Pratap Singh Chandel <shivpratap1122005@gmail.com>
2025-09-10 01:19:20 +0530

    Implemented two-pointer method

--- dsa_code/Arrays/zero_one.cpp
+++ dsa_code/Arrays/zero_one.cpp
@@ -2,29 +2,33 @@
 using namespace std;
 
 void print_array(int *arr,int size){
-      for (int i=0;i<size;i++){
-      		cout<<*(arr+i)<<" ";
-      }
-      cout<<"\n";
+    for (int i=0;i<size;i++){
+        cout<<*(arr+i)<<" ";
+    }
+    cout<<"\n";
 }
 
 int main(){
-	int arr[]={0,1,0,0,1,0};
-	int size=sizeof(arr)/sizeof(arr[0]);
-	//Will code 2 pointer
-	for (int i=0;i<size;i++){
-		if (arr[i]==1){
-      			for (int j=i;j<size;j++){
-      				if (arr[j]==0){
-      					arr[i]=arr[i]^arr[j];
-      					arr[j]=arr[i]^arr[j];
-      					arr[i]=arr[i]^arr[j];
-      					print_array(arr, size);
-					break;
-      				}
-      			}
-      		}
-	}
-	return 0;
+    int arr[]={0,1,0,0,1,0};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    //Will code 2 pointer
+    int l=0;
+    int r=size-1;
+    while (l<r){
+        if (arr[l]==0){
+            l++;
+        }
+        else if (arr[r]!=0){
+            r--;
+        }
+        else{
+            arr[l]=arr[l]^arr[r];
+            arr[r]=arr[l]^arr[r];
+            arr[l]=arr[l]^arr[r];
+            r--;
+        }
+        print_array(arr,size);
+    }
+    return 0;
 }
 
