134fa69e1509b8ed2da34bbca9c599099f2b73fa
Shiv Pratap Singh Chandel <148839982+Chandel247@users.noreply.github.com>
2026-02-10 05:55:07 +0530

    Time: 3 ms (31.73%) | Memory: 10.3 MB (24.39%) - LeetSync

--- dsa_code/125-valid-palindrome/valid-palindrome.cpp
+++ dsa_code/125-valid-palindrome/valid-palindrome.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string input="";
-        int n=s.size();
-        for (int i=0;i<n;i++){
-            int a=static_cast<unsigned int>(s[i]);
-            if ((a>=65)&&(a<=90)){
+        for (char a:s){
+            if ((a>='A')&&(a<='Z')){
                 a+=32;
-                input.push_back(static_cast<char>(a));
+                input.push_back(a);
             }
-            else if ((a>=97)&&(a<=122)){
-                input.push_back(static_cast<char>(a));
+            else if ((a>='a')&&(a<='z')){
+                input.push_back(a);
             }
-            else if ((a>=48)&&(a<=57)){
-                input.push_back(static_cast<char>(a));
+            else if ((a>='0')&&(a<='9')){
+                input.push_back(a);
             }
         }
         int start=0, end=input.size()-1;
         while (start<end){
-            if (static_cast<int>(input[start]!=static_cast<int>(input[end]))) return false;
+            if (input[start]!=input[end]) return false;
             start++;
             end--;
         }
